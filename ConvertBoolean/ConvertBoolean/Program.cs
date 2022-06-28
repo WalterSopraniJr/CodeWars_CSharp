b628e5e9eedebe1ac4d0ca310670d2052062e374
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-06-28 20:29:53 -0300

    Desafio de conversão boolean para string resolvido.

---
+++ ConvertBoolean/ConvertBoolean/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertBoolean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string boolToWord(bool word)
+            {
+                if (word == true)
+                {
+                    return "Yes";
+                }
+                else if (word == false)
+                {
+                    return "No";
+                }
+                return "Wrong entry.";
+            }
+
+            Console.WriteLine(boolToWord(true));
+            //"Yes"
+
+            Console.WriteLine(boolToWord(false));
+            //"No"
+
+            Console.Read();
+        }
+    }
+    
+}
