26bfb8c683b9f7c73e5ed997c4c9aca809372b63
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-06-29 05:30:00 -0300

    Desafio do CodeWars sobre tocar banjo concluído.

---
+++ AreYouPlayingBanjo/AreYouPlayingBanjo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreYouPlayingBanjo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreYouPlayingBanjo("Nicolas"));
+            Console.WriteLine(AreYouPlayingBanjo("Renan"));
+            Console.ReadLine();
+
+
+            string AreYouPlayingBanjo(string name)
+            {
+                char[] entry = name.ToCharArray();
+                char firstLetter = entry[0];
+                bool condition = (firstLetter == 'R' || firstLetter == 'r');
+
+                if (condition)
+                {
+                    return name + " plays banjo";
+                }
+                return name + " does not play banjo";
+            }
+        }
+    }
+}
---
+++ ReversedSequence/ReversedSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ReverseSeq(int n)
+            {
+                int[] resultado = new int[n];
+                int valor = n;
+
+                for (int i = 0;  i > n; i++)
+                {
+                    resultado[i] = valor;
+                    valor--;
+                }
+
+
+                throw new NotImplementedException();
+            }
+            public ImplementedException()
+    }
+}
