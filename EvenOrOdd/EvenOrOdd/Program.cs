d08a8f74f6762b3c1a30b001ba98e360dce3ef13
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-07-04 05:55:00 -0300

    Resolvido desafio Even or Odd.

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The number 10 is " + EvenOrOdd(10));
+
+            Console.WriteLine("The number 13 is " + EvenOrOdd(13));
+
+            Console.WriteLine("The number 22 is " + EvenOrOdd(22));
+
+            Console.WriteLine("The number 44 is " + EvenOrOdd(44));
+
+            Console.WriteLine("The number 57 is " + EvenOrOdd(57));
+
+            Console.ReadLine();
+        }
+
+        public static string EvenOrOdd(int number)
+        {
+            float result = number % 2;
+            if (result == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
---
+++ EvenOrOdd/README.md
@@ -0,0 +1,6 @@
+Even or Odd
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+<Fundamentals>
+<8 kyu>
