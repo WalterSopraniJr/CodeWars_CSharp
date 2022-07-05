8f1e0c90e989f7bfcfab19e118391213a3237ffe
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-07-05 05:32:00 -0300

    Resolvido desafio String To Number.

---
+++ ConvertStringToNumber/ConvertStringToNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringToNumber("1546"));
+
+            Console.WriteLine(StringToNumber("0000"));
+
+            Console.WriteLine(StringToNumber("1111"));
+
+            Console.WriteLine(StringToNumber("78512"));
+
+            Console.ReadLine();
+        }
+
+        public static int StringToNumber(String str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+}
