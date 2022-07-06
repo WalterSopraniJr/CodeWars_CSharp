762a4e2fd56f4534afef2f0e05c4bbe9259b2dda
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-07-06 17:59:00 -0300

    Desafio DNA RNA concluído.

---
+++ DNAtoRNAConversion/DNAtoRNAConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNAtoRNAConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+
+            string dna = "AGCU";
+
+            Console.WriteLine(DnaToRna(dna));
+            Console.Read();
+        }
+
+        public static string DnaToRna(string dna)
+        {
+            string RNA = dna.Replace("U", "T");
+            
+
+            if (string.IsNullOrEmpty(dna))
+            {
+                throw new Exception("Preencher o DNA");
+            }
+            
+            return RNA;
+        }
+
+    }
+
+}
